477d45312692903afec68c02c4c3b9c5dcca754e
Leandro Lucas dos Santos <leandrosantos182@gmail.com>
2021-07-22 16:22:15 -0300

    Finalizando todas funcionalidades dos botões

--- src/route.js
+++ src/route.js
@@ -4,11 +4,15 @@ const questionController = require('./controller/questionController')
 const roomController = require('./controller/roomController')
 
 route.get('/', (req, res) => res.render('index', { page: 'enter-room' }))
-route.get('/room/:room', (req, res) => res.render('room'))
 route.get('/create-pass', (req, res) =>
   res.render('index', { page: 'create-pass' })
 )
-route.post('/question/:room/:question/:action', questionController.index)
+
 route.post('/create-room', roomController.create)
+route.get('/room/:room', roomController.open)
+route.post('/enter-room', roomController.enter)
+
+route.post('/question/create/:room', questionController.create)
+route.post('/question/:room/:question/:action', questionController.index)
 
 module.exports = route
